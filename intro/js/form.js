13819e338ce7dc2b1741da86b39064cec1d1b971
Matteo Polizeli <polizeli.contato@gmail.com>
2022-05-03 16:30:21 -0300

    Progresso - 03/05/2022
    alterações, funcionalidades novas

--- intro/js/form.js
+++ intro/js/form.js
@@ -1,22 +1,42 @@
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
-    event.preventDefault();
+    botaoAdicionar.addEventListener("click", function(event){
+        event.preventDefault();
 
-    var form = document.querySelector("#form-adiciona");
+        var form = document.querySelector("#form-adiciona");
 
-    // extrai informações do paciente do form
-    var paciente = obtemPacienteDoForm(form);
+        // extrai informações do paciente do form
+        var paciente = obtemPacienteDoForm(form);
 
-    // cria a tr e td do do paciente
-    var pacienteTr = montaTr(paciente);
+        // cria a tr e td do do paciente
+        var pacienteTr = montaTr(paciente);
+        
+        var erros = validaPaciente(paciente);
+        console.log(erros);
 
-    // adiciona o paciente na tabela
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
+        if(erros.length > 0){
+         exibeMensagensDeErro(erros);
+            return;         
+        }
+        // adiciona o paciente na tabela
+        var tabela = document.querySelector("#tabela-pacientes");
+        tabela.appendChild(pacienteTr);
 
-    form.reset();
+        form.reset();
+        var mensagensErro = document.querySelector("#mensagens-erro")
+        mensagensErro.innerHTML = ""
 });
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro")
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
 function obtemPacienteDoForm(form){
     
@@ -50,3 +70,30 @@ function montaTd(dado, classe){
     td.classList.add(classe);
     return td;
 }
+
+function validaPaciente(paciente){
+
+    var erros = [];
+
+    if(paciente.nome.length == 0)  
+        erros.push("O nome não pode ser em branco")
+    
+    if (!validaPeso(paciente.peso)) 
+        erros.push("Peso é inválido");
+     
+    if(!validaAltura(paciente.altura)) 
+        erros.push("Altura é inválida");
+    
+    if(paciente.gordura.length == 0)
+        erros.push("A gordura não pode ser em branco")
+    
+    if(paciente.peso.length == 0)
+        erros.push("O peso não pode ser em branco")
+    
+    if(paciente.altura.length == 0)
+        erros.push("A altura não pode ser em branco")
+
+
+    return erros;
+}
+
