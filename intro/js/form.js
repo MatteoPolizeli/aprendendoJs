fb42f8aa5a3cac722f8214f64d96c1bf0d3d0649
Matteo Polizeli <polizeli.contato@gmail.com>
2022-05-03 10:21:08 -0300

    MaisFunctions
    -- Isolamos responsabilidades em functions e melhoramos algumas estruturas, como a de criação

--- intro/js/form.js
+++ intro/js/form.js
@@ -9,11 +9,13 @@ botaoAdicionar.addEventListener("click", function(event){
     var paciente = obtemPacienteDoForm(form);
 
     // cria a tr e td do do paciente
-    var pacienteTr montaTr(paciente);
+    var pacienteTr = montaTr(paciente);
 
     // adiciona o paciente na tabela
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
+
+    form.reset();
 });
 
 function obtemPacienteDoForm(form){
@@ -25,32 +27,26 @@ function obtemPacienteDoForm(form){
     gordura: form.gordura.value,
     imc: calculaImc(form.peso.value, form.altura.value)
 }
-    return paciente;
-   
+    return paciente;   
 
 }
 
 function montaTr(paciente){
     var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
 
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = paciente.nome;
-    pesoTd.textContent = paciente.peso;
-    alturaTd.textContent = paciente.altura;
-    gorduraTd.textContent = paciente.gordura;
-    imcTd.textContent = paciente.imcTd;
-
-    pacienteTr.appendChild(nomeTd)
-    pacienteTr.appendChild(pesoTd)
-    pacienteTr.appendChild(alturaTd)
-    pacienteTr.appendChild(gorduraTd)
-    pacienteTr.appendChild(imcTd)
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
     return pacienteTr;
 }
-inter
+
+function montaTd(dado, classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+    return td;
+}
