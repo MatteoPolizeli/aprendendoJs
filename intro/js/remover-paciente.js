bf5fd9775437c79a1d42ce93e1caceca7d39ead7
Matteo Polizeli <polizeli.contato@gmail.com>
2022-05-04 16:19:39 -0300

    Aula 8 - alterações
    Projeto a partir da aula 8
    RegExp, filtragem, Remoção, mais dinâmica ao sisteminha.

--- intro/js/remover-paciente.js
+++ intro/js/remover-paciente.js
@@ -1,2 +1,9 @@
-var pacientes = document.querySelectorAll(".paciente")
-console.log(pacientes);
+var tabela = document.querySelector("table")
+
+tabela.addEventListener("dblclick", function(event){
+    
+    event.target.parentNode.classList.add("fadeOut")
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500);
+})
